0525bbe4976e2c23547eb35e7f38599ec0e340ac
gds1110 <45266970+gds1110@users.noreply.github.com>
2024-04-14 01:52:38 +0900

    need third

--- Assets/Scripts/Controllers/TPSCharacterController.cs
+++ Assets/Scripts/Controllers/TPSCharacterController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class TPSCharacterController : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _characterBody;
+    [SerializeField]
+    private Transform _cameraArm;
+
+    Animator _anim;
+    
+    public float _speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim=_characterBody.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
+        LookAround();
+        Move();
+    }
+    public void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        // _anim.SetBool("isMove", isMove);
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(_cameraArm.forward.x, 0f, _cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(_cameraArm.right.x, 0f, _cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            _characterBody.forward = lookForward;
+            transform.position += moveDir * Time.deltaTime * _speed;
+        }
+
+    }
+    private void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = _cameraArm.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+        if(x<180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 355f, 361f);
+        }
+        _cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
     }
 }
